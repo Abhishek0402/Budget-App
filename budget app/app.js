8282a58bc100b83fe45ef533c4b31fdacad64eec
AbhishekGupta0402 <abhishekedu4979@gmail.com>
2019-10-05 20:51:19 +0530

    income and expense add with button click feature added

--- budget app/app.js
+++ budget app/app.js
@@ -66,7 +66,9 @@ var budgetController = (function() {
          inputType: '.add__type',
          inputDescription: '.add__description',
          inputValue: '.add__value',
-         inputBtn: '.add__btn'
+         inputBtn: '.add__btn',
+         incomeContainer: '.income__list',
+         expensesContainer: '.expenses__list'
      };
     return {
         getInput: function() {
@@ -76,6 +78,30 @@ var budgetController = (function() {
                 value :  document.querySelector(DOMstrings.inputValue).value
 
             };
+         },
+
+         addListItem: function(obj, type) {
+            var html,newHtml;
+             
+            //create HTML strings with place holder text
+
+            if(type === 'inc'){
+             element = DOMstrings.incomeContainer;
+
+html = '<div class="item clearfix" id="income-%id%"><div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">%value%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>';
+      }
+   else if(type === 'exp'){  
+       element = DOMstrings.expensesContainer;      
+html = '<div class="item clearfix" id="expense-%id%"><div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">%value%</div><div class="item__percentage">21%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>';
+   }
+
+           //Replace the placeholder text with some actual data
+          newHtml = html.replace('%id', obj.id);
+          newHtml = newHtml.replace('%description%', obj.description);
+          newHtml = newHtml.replace('%value', obj.value);
+
+            //Insert the HTML into the DOM // we are going to insert json html method
+             document.querySelector(element).insertAdjacentHTML('beforeend', newHtml);
          },
          getDOMstrings: function(){
              return DOMstrings;
@@ -112,7 +138,7 @@ var budgetController = (function() {
       //2. add the input to budget controller
          newItem =  budgetCtrl.addItem(input.type, input.description, input.value);
        //3. add the items to the ui controller
-
+         UICtrl.addListItem(newItem, input.type);
       //4. calculate budget
 
       //5. display the budget(changes on the ui)
